a601b2acaeca15bfd9815c4a55f8e4583afdf060
Igor <cheremisinig@mail.ru>
2020-10-06 16:53:30 +0700

    Add Test words count equal

--- test/test.c
+++ test/test.c
@@ -19,3 +19,27 @@ CTEST(file_check, file_dosnt_exist)
     int expected = 0;
     ASSERT_EQUAL(expected, result);
 }
+CTEST(sort_check, words_count_equal)
+{
+    char strings[100][100], str[200];
+    int expected = 3;
+    int result;
+    FILE* f = open_file("test.txt", "r");
+    if (f == NULL) {
+        return NULL;
+    }
+    result = check_file(f, strings);
+    ASSERT_EQUAL(expected, result);
+}
+CTEST(sort_check, words_count_not_equal)
+{
+    char strings[100][100], str[200];
+    int expected = 12;
+    int result;
+    FILE* f = open_file("test.txt", "r");
+    if (f == NULL) {
+        return NULL;
+    }
+    result = check_file(f, strings);
+    ASSERT_EQUAL(expected, result);
+}
