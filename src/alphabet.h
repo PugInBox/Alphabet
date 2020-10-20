1e832b27aacc94ac6fb42996d2458c8f90cb487e
PugInBox <fereuda@mail.ru>
2020-10-20 19:33:10 +0700

    Removed open_file function

--- src/alphabet.h
+++ src/alphabet.h
@@ -1,16 +1,16 @@
 #ifndef ALPHABET_H
 #define ALPHABET_H
 
+#include <locale.h>
 #include <malloc.h>
+#include <stdlib.h>
 
 #define ERROR_CONSOLE_ARGUMENT -10
 #define ERROR_OPEN_FILE -1
 #define OK 0
-
 #define MAX_LENGTH_STRING 400
 #define MAX_LENGTH_FILE 1000
 
-FILE* open_file(char* file_name, char* mode);
 void UpperToLower(char* string);
 int check_file(FILE* file, char strings[][MAX_LENGTH_STRING]);
 void output(char strings[][MAX_LENGTH_STRING], int count);
