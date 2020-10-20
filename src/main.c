839965f5553664a61b376cf19718e524e960be63
PugInBox <fereuda@mail.ru>
2020-10-20 19:33:32 +0700

    Removed open_file function

--- src/main.c
+++ src/main.c
@@ -1,23 +1,20 @@
 #include "alphabet.h"
+#include <locale.h>
 #include <malloc.h>
 #include <stdio.h>
 
 int main(int argc, char** argv)
 {
-    //проверяем, что есть аргумент с названием файла
     if (argc == 2) {
-        char strings[MAX_LENGTH_FILE][MAX_LENGTH_STRING];
-        FILE* f = open_file(argv[1], "r");
+        char strings[MAX_LENGTH_STRING][MAX_LENGTH_STRING];
+        FILE* f = fopen(argv[1], "r");
         if (f == NULL) {
             return ERROR_OPEN_FILE;
         }
-        //Сортируем слова
         int count = check_file(f, strings);
-        //Выводим строки
         output(strings, count);
         return OK;
     } else {
-        //Если аргумента нет, выводим ошибку
         printf("Введите путь до файла\nexample: ./bin/main input.txt");
         return ERROR_CONSOLE_ARGUMENT;
     }
